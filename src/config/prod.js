50a2e486d69b96490317f99266528a0d61c64e62
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-09-24 23:56:56 +0600

    a

--- src/config/prod.js
+++ src/config/prod.js
@@ -2,6 +2,6 @@
 
 export default {
   // Over write default settings here...
-  host: process.env.NODE_HOST || 'https://rocket-firm-unsplash-client.herokuapp.com',
+  host: process.env.NODE_HOST || 'localhost',
   port: process.env.PORT || '4040',
 };
