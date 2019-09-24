de7db70de5ad23f496415fad9b849f2bbbe9c9a2
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-09-24 18:54:02 +0600

    added conditional base url

--- src/actions/axios.js
+++ src/actions/axios.js
@@ -1,7 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/v1/';
+let API_URL;
+
+if (!process.env.NODE_ENV === 'development') {
+  API_URL = 'https://rocket-firm-unsplash-server.herokuapp.com/api/v1/';
+} else {
+  API_URL = 'http://localhost:3000/api/v1/';
+}
 
 export const apiRequest = axios.create({
   baseURL: API_URL,
