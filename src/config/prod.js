4357df68830dc973d163ba148e3b401039712985
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-09-25 14:17:34 +0600

    a

--- src/config/prod.js
+++ src/config/prod.js
@@ -2,6 +2,6 @@
 
 export default {
   // Over write default settings here...
-  host: process.env.NODE_HOST || 'localhost',
+  host: process.env.NODE_HOST || '0.0.0.0',
   port: process.env.PORT || '4040',
 };
